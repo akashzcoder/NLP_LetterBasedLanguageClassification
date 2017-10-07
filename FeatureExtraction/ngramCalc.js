94a3b129d8b139fc8c08c6addafdb35667a4fa69
Akash Singh <akashsingh09.03.93@gmail.com>
2017-10-07 09:11:34 -0400

    minor fixes for term frequency

--- FeatureExtraction/ngramCalc.js
+++ FeatureExtraction/ngramCalc.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+var csvdata = require('csvdata');
 var nGram = require('n-gram');
 var LineByLineReader = require('line-by-line'),
     lr = new LineByLineReader('French_v1.txt');
@@ -12,9 +13,14 @@ lr.on('error', function (err) {
 lr.on('line', function (line) {
 	// 'line' contains the current line without the trailing newline character.
     total = line.toLowerCase();
-    console.log(nGram(1)(total));
-    console.log(nGram.bigram(total)); // ['n-', '-g', 'gr', 'ra', 'am'] 
-    console.log(nGram.trigram(total));
+    var gram = nGram(1)(total);
+    console.log(gram);
+    csvdata.write('./gram1.csv', gram, {header: '1gram'});
+    var twoGram = nGram.bigram(total);
+    csvdata.write('./gram2.csv', twoGram, {header: '2gram'}); // ['n-', '-g', 'gr', 'ra', 'am'] 
+    var threeGram = nGram.trigram(total);
+    csvdata.write('./gram3.csv', threeGram, {header: '3gram'});
+    // console.log(nGram.trigram(total));
 });
 
 lr.on('end', function () {
