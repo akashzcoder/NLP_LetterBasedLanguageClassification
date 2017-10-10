d9667b41dc4820b8c115096c537c32b759a8bebd
Akash Singh <akashsingh09.03.93@gmail.com>
2017-10-10 12:51:12 -0400

    Stop word removal code with the output
    Stop words have been removed using the npm library. Please use nodejs to run the code.
    
    Former-commit-id: 4848d4414da9f4ebe8f41ace830474eac5999907

---
+++ FeatureExtraction/StopWord removal/SpanishSW.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+var unique = require('array-unique');
+var csvdata = require('csvdata');
+var writeFile = require('write');
+var createFile = require('create-file');
+var unique = require('array-unique');
+var HashMap = require('hashmap');
+sw = require('stopword')
+var LineByLineReader = require('line-by-line'),
+    lr = new LineByLineReader('Spanish_v1.txt');
+
+lr.on('line', function (line) {
+	// 'line' contains the current line without the trailing newline character.
+    total = line.toLowerCase();
+    var oldString = total.split(' ');
+    var newString = sw.removeStopwords(oldString,sw.es) + '\n';
+    fs.appendFile('SpanishSW.txt', newString, (err) => {  
+        // throws an error, you could also catch it here
+        if (err) throw err;
+
+        // success case, the file was saved
+        // console.log('saved!');
+    });
+
+    // console.log(charArr);
+});
+
+lr.on('end', function () {
+
+
+});
